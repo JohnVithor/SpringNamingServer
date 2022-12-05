213ba2d9d271cec8b6efe3ecd5741a343e86349e
JohnVithor <jv.venceslau.c@gmail.com>
2022-12-05 08:43:12 -0300

    adiciona formatação de arquivos

--- src/main/java/ufrn/imd/jv/springnamingserver/SpringNamingServerApplication.java
+++ src/main/java/ufrn/imd/jv/springnamingserver/SpringNamingServerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class SpringNamingServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringNamingServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNamingServerApplication.class, args);
+    }
 
 }
